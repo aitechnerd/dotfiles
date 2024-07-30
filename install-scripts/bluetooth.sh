ce591e3401a3c1385dc0b062f5ac56b1ab8d8726
Sergey Belov <belov.ss@gmail.com>
2024-07-29 22:49:07 -0600

    fix bluetooth installation

--- install-scripts/bluetooth.sh
+++ install-scripts/bluetooth.sh
@@ -6,8 +6,8 @@ if ask_yes_no "Do you want to install Bluetooth utils?"; then
     sudo pacman -S --noconfirm bluez bluez-utils
     sleep 1
     if ask_yes_no "'scan on' and then 'pair MAC_address'. Or use Ready to connect bluetooth devices?"; then
-        bluetoothctl
         systemctl enable bluetooth.service
         systemctl start bluetooth.service
+        bluetoothctl
     fi
 fi
